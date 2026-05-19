6d49668be5922cb163a4cc1a6ccb987b58bb81e4
ramontsantos-trindade <ramon_t_santos@estudante.sesisenai.org.br>
2026-05-19 08:34:42 -0300

    up - outro up java

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ function resultado() {
         display.value = 'errado'
     }
 }
+
+function Remover() {
+    const display = document.getElementById('display')
+    display.value = display.value.slice(0,-1)
+}
+function resultado() {
+    let display = document.getElementById("display");
+
+    try {
+        let conta = display.value.replace(",", "."); // pra funcionar com vírgula
+        let resultado = eval(conta);
+
+        if (resultado == 67) {
+            display.value = "feijao com farinha";
+        } else {
+            display.value = resultado;
+        }
+
+    } catch {
+        display.value = "Erro";
+    }
+}
